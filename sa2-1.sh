155b71c327cb1c646761ea79f4657d728b55791b
DongGuaLemon <g0970216929@gmail.com>
2020-07-31 20:15:11 +0800

    fix total

--- sa2-1.sh
+++ sa2-1.sh
@@ -1,2 +1,2 @@
-ls -ARl | grep '^[-|d]' | sort -n -r -k 5 | awk '$1 ~ /^[-|d]/{all++}{if(all<= 5) {print all ":" $5,$9};} $1 ~ /^d/{dir++}END{if(dir==0) print "Dir num:" 0;else print "Dir num:" dir;} $1 ~ /^-/{file++}END{if(file==0) print "File num:" 0;else print "File num" file;} END{print "Total:" NR}' 
+ls -ARl | grep '^[-|d]' | sort -n -r -k 5 | awk '$1 ~ /^[-|d]/{all++}{if(all<= 5) {print all ":" $5,$9};} $1 ~ /^d/{dir++}END{if(dir==0) print "Dir num:" 0;else print "Dir num:" dir;} $1 ~ /^-/{{file++;}{total=total+$5;}}END{if(file==0) print "File num:" 0;else print "File num" file;} END{print "Total:" total}' 
 
