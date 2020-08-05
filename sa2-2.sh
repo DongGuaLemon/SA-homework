ff6d3bff737c8de972a8d56ecd90e5e95847378f
LiZongwei <31680128+DongGuaLemon@users.noreply.github.com>
2020-08-05 17:09:03 +0800

    Add files via upload

--- sa2-2.sh
+++ sa2-2.sh
@@ -9,7 +9,7 @@ main (){
 			3)
 				net;;
 			4)	
-				file;;
+				files;;
 			*)
 				echo "no such choice";;
 		esac
@@ -18,21 +18,36 @@ cpu (){
 	cpumachine=$(sysctl -a hw.machine | awk '{$1= "";print}')
 	cpumodel=$(sysctl -a | egrep -i 'hw.model' | awk '{$1= "";print}')
 	cpucore=$(sysctl -a hw.ncpu | awk '{$1= "";print}')
-	dialog --title "CPU INFO" --clear --msgbox \
+	dialog --stdout --title "CPU INFO" --clear --msgbox \
 		"CPU Model: ${cpumodel}\n\nCPU Machine: ${cpumachine}\n\nCPU Core: ${cpucore}" 20 60
+	if [ $?==0 ];then
+		main
+	fi	
 }
 mem (){
 	memreal=$(sysctl -a hw.realmem | awk '{print $2/1024/1024/1024}')
 	memfree=$(sysctl -a hw.usermem | awk '{print $2/1024/1024/1024}')
 	memused=$(echo $memreal $memfree | awk '{print $1-$2}')
 	usegauge=$(echo $memused $memreal | awk '{print int($1/$2*100)}')	
-	dialog --title "Memory Info and Usage" --clear --gauge \
-		"Total: ${memreal} GB\nUsed: ${memused} GB\nFree: ${memfree} GB" 20 60 ${usegauge}
 	
+		dialog --stdout --title "Memory Info and Usage" --clear --gauge \
+	 "Total: ${memreal} GB\nUsed: ${memused} GB\nFree: ${memfree} GB" 20 60 ${usegauge} 2>&1 >/dev/tty
+	read -r input
+	echo $input
+	while true; do
+		
+		read -r input
+	        if [ "$input" == "a" ]; then 
+		        echo "hello"
+			exit 0           
+		fi
+	done
+
 }
 net (){
 	allnet=$(ifconfig -a | sed -E 's/[[:space:]:].*//;/^$/d'| awk '{print $0 " \"*\""}')
-	choice=$(dialog --menu "test" 20 50 2 ${allnet} 2>&1 >/dev/tty)
+	indexnet=$(ifconfig -a | sed -E 's/[[:space:]:].*//;/^$/d' | awk '$0{dir++}END{print dir}')
+	choice=$(dialog --menu "Net" 20 50 ${indexnet} ${allnet} 2>&1 >/dev/tty)
 	if [ -z $choice ]; then
 		echo $(clear)
 	else
@@ -44,13 +59,42 @@ netinfo (){
 	ipinfo=$(ifconfig $choice | awk '$0 ~ /inet /{print $2}')
 	maskinfo=$(ifconfig $choice | awk '$0 ~ /netmask /{print $4}')
 	macinfo=$(ifconfig $choice | awk '$0 ~ /ether /{print $2}')
-	dialog --title "Interface Name:${choice}" --msgbox \
+	dialog --stdout --title "Interface Name:${choice}" --msgbox \
 		"IPv4___: ${ipinfo}\nNetmask: ${maskinfo}\nMac____: ${macinfo}" 20 60
+	if [ $?==0 ];then
+		net
+	fi	
+}
+files (){
+	pwd=$(pwd)
+	files=$(ls -a $pwd | xargs file -i | awk '{print $1 " " $2}' | sed 's/\://g')
+	fileindex=$(ls -a $pwd | xargs file -i | awk '$0{dir++}END{print dir}')
+	choice=$(dialog --menu "File browser ${pwd}" 20 50 ${fileindex} ${files} 2>&1 >/dev/tty)
+	if [ -z $choice ];then
+		echo $(clear)
+	else
+		fileinfo
+	fi	
+}
+fileinfo (){
+	pwd=$(pwd)
+	#fileinfo= echo $pwd/$choice | xargs file | awk '{print $1 = "";print}'
+	fileinfo=`file $pwd/$choice -b`
+	filesize=$(ls -lah $choice | awk '{ print $5}')
+	if [ "$fileinfo" != "directory" ]; then
+		#vim ${choice}
+		#echo finish && exit 0
+		select=$(dialog --stdout --extra-button --extra-label "edit" --ok-label "ok" --msgbox \
+			"File Name: ${choice}\n\nFile Info: ${fileinfo}\n\nFile Size: ${filesize}" 20 40 2>&1 >/dev/tty)
+		if [ $? == 3 ];then
+			vim $choice
+		fi	
+		files
+	else
+		cd $choice
+		files
+	fi	
+}
 
-}
-file (){
-	file=$(ls -f|awk '{print $0}')
-	echo $(file $file)
-}
+#fileinfo
 main
-
